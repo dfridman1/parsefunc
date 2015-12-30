1a4b4e3a8495adae84dd03e4a83a6ec20c858753
Denys Fridman <fridman1992@mail.ru>
2015-12-30 22:59:57 +0200

    add syntax_tree decorator

--- prim.py
+++ prim.py
@@ -1,7 +1,22 @@
 from state import initialParseState
+from state import isParseSuccess, parseSuccessTree, setParseSuccessTree
 
 
 
+
+def syntax_tree(transform):
+    '''Decorator for the user to apply a 'transform' function to the tree
+    returned by the parser.'''
+    def func(parser):
+        def processor(state):
+            newstate = parser(state)
+            if isParseSuccess(newstate):
+                tree = parseSuccessTree(newstate)
+                return setParseSuccessTree(newstate, transform(tree))
+            else:
+                return newstate
+        return processor
+    return func
 
 
 
