b275aec44a602b18d7579f600a3f88c60f4a3812
Denys Fridman <fridman1992@mail.ru>
2016-01-04 12:06:10 +0200

    rewrite simple_arithm.py

--- examples/simple_arithm.py
+++ examples/simple_arithm.py
@@ -6,10 +6,7 @@ sys.path.append('..')
 from parsec import *
 
 
-op = lambda ch: char(ch) >= (lambda x: whiteSpace >> lift(x))
-
-plus = op('+')
-mult = op('*')
+plus, mult = char('+'), char('*')
 
 
 @syntax_tree(sum)
@@ -17,28 +14,23 @@ def parseExpr(s):
     return sepBy(parseTerm, plus)(s)
 
 
-@Parser
-def parseFactor(s):
-    return (parens(parseExpr) | parseNatural)(s)
-
-
-@Parser
-def parseNatural(s):
-    return (natural >= (lambda num: many(space) >> lift(num)))(s)
-
-
 product = lambda xs: reduce(lambda x, y: x * y, xs, 1)
 
 
-@syntax_tree(lambda x: product(x) if isinstance(x, list) else x)
+@syntax_tree(product)
 def parseTerm(s):
-    return (tryP(sepBy(parseFactor, mult)) | parseFactor)(s)
+    return sepBy(parseFactor, mult)(s)
+
+
+@Parser
+def parseFactor(s):
+    return between(whiteSpace, whiteSpace, parens(parseExpr) | integer)(s)
 
 
 
 
 if __name__ == '__main__':
-    
+
     expr = raw_input('enter expression:\n')
 
-    print parse(parseExpr, expr, sourceName='somefile.py')
+    print parse(parseExpr, expr, sourceName='simple_arithm.py')
